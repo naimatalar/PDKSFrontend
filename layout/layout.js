a38a249a4b334c6445a61c434da7d62f75ebc435
naim <atalar_n@hotmail.com>
2026-02-25 10:49:27 +0300

    fhjf

--- layout/layout.js
+++ layout/layout.js
@@ -536,13 +536,10 @@ function Layout({ children, permissionControl = true, loadingContent = false, pa
 
                         <div className="navbar-collapse collapse" id="navbar-footer">
                             <span className="navbar-text">
-                                &copy; 2022 <b>KYC AŞ </b>Tarafından Projelendirilmiştir
+                                &copy; 2022 <b>ARMA BİLİŞİM  </b>Tarafından Projelendirilmiştir
                             </span>
 
                             <ul className="navbar-nav ml-lg-auto">
-                                <li className="nav-item"><a href="https://kopyov.ticksy.com/" className="navbar-nav-link" target="_blank"><i className="icon-lifebuoy mr-2"></i> Support</a></li>
-                                <li className="nav-item"><a href="http://demo.interface.club/limitless/docs/" className="navbar-nav-link" target="_blank"><i className="icon-file-text2 mr-2"></i> Docs</a></li>
-                                <li className="nav-item"><a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328?ref=kopyov" className="navbar-nav-link font-weight-semibold"><span className="text-pink-400"><i className="icon-cart2 mr-2"></i> Purchase</span></a></li>
                             </ul>
                         </div>
                     </div>
